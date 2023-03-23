b8acb82860f3e17dad3b87a7a14b6ea9adda0f70
Bruno Arruda <bruno.ogarruda@gmail.com>
2023-03-22 21:09:21 -0300

    Day 3 - Classes 32-35

--- 03-PrimeiraTelaCadastroCurso/03-PrimeiraTelaCadastroCurso/ViewController.swift
+++ 03-PrimeiraTelaCadastroCurso/03-PrimeiraTelaCadastroCurso/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.keyboardType = .default
+        emailTextField.keyboardType = .emailAddress
+        passwordTextField.keyboardType = .default
+        passwordTextField.isSecureTextEntry = true
+        
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        registerButton.isEnabled = false
+        registerButton.setTitleColor(.white.withAlphaComponent(0.4), for: .disabled)
+        registerButton.setTitleColor(.white, for: .normal)
     }
 
+    @IBAction func tappedRegisterButton(_ sender: Any) {
+        print("Botão está funcionando")
+    }
+    
+    func validateTextField() {
+        if nameTextField.text != "" && emailTextField.text != "" && passwordTextField.text != "" {
+            registerButton.isEnabled = true
+        } else {
+            registerButton.isEnabled = false
+        }
+    }
+}
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.blue.cgColor
+        textField.layer.borderWidth = 1
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 0
+        validateTextField()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
