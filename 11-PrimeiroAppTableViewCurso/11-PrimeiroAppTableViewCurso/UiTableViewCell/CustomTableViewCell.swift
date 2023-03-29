f951836b34d0cfc6270535ca0d45d57129bfdd77
Bruno Arruda <bruno.ogarruda@gmail.com>
2023-03-29 20:16:06 -0300

    Day 7 - Classes 49-50

--- 11-PrimeiroAppTableViewCurso/11-PrimeiroAppTableViewCurso/UiTableViewCell/CustomTableViewCell.swift
+++ 11-PrimeiroAppTableViewCurso/11-PrimeiroAppTableViewCurso/UiTableViewCell/CustomTableViewCell.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    static let identifier: String = "CustomTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: self.identifier, bundle: nil)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        titleLabel.text = "Bruno"
+    }
+    
+    func setupCell(title: String) {
+        titleLabel.text = title
     }
     
 }
--- 11-PrimeiroAppTableViewCurso/11-PrimeiroAppTableViewCurso/ViewController.swift
+++ 11-PrimeiroAppTableViewCurso/11-PrimeiroAppTableViewCurso/ViewController.swift
@@ -11,16 +11,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var listName: [String] = ["Bruno", "Caio", "Matheus", "Pedro", "Felipe"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.configTableView()
     }
 
     func configTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(CustomTableViewCell.nib(), forCellReuseIdentifier: CustomTableViewCell.identifier)
     }
-    
     
 }
 
@@ -29,11 +32,18 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return listName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath)
+            as? CustomTableViewCell
+        cell?.setupCell(title: listName[indexPath.row])
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(listName[indexPath.row])
     }
     
     
